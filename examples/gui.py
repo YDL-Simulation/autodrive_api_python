7481b7857e73f68fb348699a5ff1662ec56f52f0
sky3141 <sky3141@qq.com>
2025-04-05 17:00:04 +0800

    refactor: 大规模重构

--- examples/gui.py
+++ examples/gui.py
@@ -10,9 +10,22 @@ from multiprocessing.connection import Connection
 import time
 from enum import Enum, auto
 import math
+from dataclasses import dataclass
 from scipy.spatial import KDTree
+from metacar import (
+    Vector2,
+    SceneStaticData,
+    SimCarMsg,
+    LineType,
+)
 
-from scene_api import SceneInfo, Vector2, ObjectInfo, RoadLineType, RoadLineInfo
+
+@dataclass
+class LineInfo:
+    """绘制用的车道线信息"""
+
+    type: LineType
+    points: list[Vector2]
 
 
 class Message(Enum):
@@ -21,35 +34,32 @@ class Message(Enum):
 
 
 def tk_process_func(
-    conn: Connection, road_lines: list[RoadLineInfo], refresh_interval: int
+    conn: Connection, scene_static_data: SceneStaticData, refresh_interval: int
 ):
     ROADLINE_COLOR_MAP = {
-        RoadLineType.NULL: "black",  # 没有类型
-        RoadLineType.MIDDLE_LINE: "dark orange",  # 中线
-        RoadLineType.SIDE_LINE: "black",  # 边线
-        RoadLineType.SOLID_LINE: "black",  # 实线
-        RoadLineType.STOP_LINE: "dark red",  # 停车线
-        RoadLineType.ZEBRA_CROSSING: "dark blue",  # 斑马线
-        RoadLineType.DASH_LINE: "grey",  # 虚线
+        LineType.MIDDLE_LINE: "dark orange",  # 中线
+        LineType.SIDE_LINE: "black",  # 边线
+        LineType.SOLID_LINE: "black",  # 实线
+        LineType.STOP_LINE: "dark red",  # 停车线
+        LineType.ZEBRA_CROSSING: "dark blue",  # 斑马线
+        LineType.DASH_LINE: "grey",  # 虚线
     }
 
-    def update(scene_info: SceneInfo):
-        string_vars[0].set("{:.3f}".format(scene_info.vehicle_control.throttle))
-        string_vars[1].set("{:.3f}".format(scene_info.vehicle_control.brake))
-        string_vars[2].set("{:.3f}".format(scene_info.vehicle_control.steering))
-        string_vars[3].set(scene_info.vehicle_control.gear.name)
-        string_vars[4].set("{:.3f}".format(scene_info.main_vehicle_speed))
-        string_vars[5].set("{:.2f}".format(scene_info.main_vehicle_info.pos.x))
-        string_vars[6].set("{:.2f}".format(scene_info.main_vehicle_info.pos.y))
-        string_vars[7].set(
-            "{:.2f}".format(math.degrees(scene_info.main_vehicle_info.yaw) % 360.0)
-        )
+    def update(msg: SimCarMsg):
+        string_vars[0].set("{:.3f}".format(msg.data_main_vehicle.throttle))
+        string_vars[1].set("{:.3f}".format(msg.data_main_vehicle.brake))
+        string_vars[2].set("{:.3f}".format(msg.data_main_vehicle.steering))
+        string_vars[3].set(msg.data_main_vehicle.gear.name)
+        string_vars[4].set("{:.3f}".format(msg.data_main_vehicle.speed))
+        string_vars[5].set("{:.2f}".format(msg.data_gnss.pose_gnss.pos_x))
+        string_vars[6].set("{:.2f}".format(msg.data_gnss.pose_gnss.pos_y))
+        string_vars[7].set("{:.2f}".format(msg.data_gnss.pose_gnss.ori_z))
         # clear canvas
         map_canvas.delete("all")
         canvas_width = map_canvas.winfo_width()
         canvas_height = map_canvas.winfo_height()
         main_vehicle_pos = Vector2(
-            scene_info.main_vehicle_info.pos.x, scene_info.main_vehicle_info.pos.y
+            msg.data_gnss.pose_gnss.pos_x, msg.data_gnss.pose_gnss.pos_y
         )
 
         SCALE = 10
@@ -63,10 +73,11 @@ def tk_process_func(
             center_pos: Vector2,
             length: float,
             width: float,
-            rotate_rad: float,
+            ori_z: float,
             outline_color: str,
         ):
             """绘制带有旋转角度的矩形。"""
+            rotate_rad = -math.radians(ori_z)
             a = Vector2(length / 2, width / 2)
             b = Vector2(length / 2, -width / 2)
             c = Vector2(-length / 2, -width / 2)
@@ -78,37 +89,34 @@ def tk_process_func(
             # 绘制无填充的多边形
             map_canvas.create_polygon(a, b, c, d, fill="", outline=outline_color)
 
-        def draw_object(obj: ObjectInfo, color: str):
-            pos = Vector2(obj.pos.x, obj.pos.y)
-            length = obj.length
-            width = obj.width
-            yaw = obj.yaw
-            draw_rectangle(
-                pos,
-                length,
-                width,
-                yaw,
-                color,
-            )
-
         # 绘制主车
-        draw_object(scene_info.main_vehicle_info, "green")
+        draw_rectangle(
+            main_vehicle_pos,
+            msg.data_main_vehicle.length,
+            msg.data_main_vehicle.width,
+            msg.data_gnss.pose_gnss.ori_z,
+            "green",
+        )
 
         # 绘制障碍物
-        for obstacle in scene_info.obstacles:
-            draw_object(obstacle, "red")
+        for obstacle in msg.obstacle_entry_list:
+            draw_rectangle(
+                Vector2(obstacle.pos_x, obstacle.pos_y),
+                obstacle.length,
+                obstacle.width,
+                obstacle.ori_z,
+                "red",
+            )
 
         # 绘制行驶路线
-        trajectory_points = [
-            convert_pos(Vector2(pt.x, pt.y)) for pt in scene_info.trajectory.points
-        ]
+        trajectory_points = [convert_pos(pt.to_vector2()) for pt in msg.trajectory]
         if len(trajectory_points) > 1:
             map_canvas.create_line(trajectory_points, fill="blue")
 
         # 绘制车道线
         radius = math.hypot(canvas_width, canvas_height) / 2 / SCALE
-        min_idx = [-1] * len(road_lines)
-        max_idx = [-1] * len(road_lines)
+        min_idx = [-1] * len(line_list)
+        max_idx = [-1] * len(line_list)
         point_idxs = kdtree.query_ball_point(
             (main_vehicle_pos.x, main_vehicle_pos.y), radius
         )
@@ -120,42 +128,26 @@ def tk_process_func(
             else:
                 min_idx[rid] = min(min_idx[rid], pid)
                 max_idx[rid] = max(max_idx[rid], pid)
-        for road_line_idx, road_line in enumerate(road_lines):
-            cur_min_idx = min_idx[road_line_idx]
-            cur_max_idx = max_idx[road_line_idx]
+        for line_idx, line in enumerate(line_list):
+            cur_min_idx = min_idx[line_idx]
+            cur_max_idx = max_idx[line_idx]
             if cur_min_idx == -1:
                 continue
-            if road_line.type == RoadLineType.ZEBRA_CROSSING:
-                points = [convert_pos(Vector2(pt.x, pt.y)) for pt in road_line.points]
+            if line.type == LineType.ZEBRA_CROSSING:
+                points = [convert_pos(Vector2(pt.x, pt.y)) for pt in line.points]
                 map_canvas.create_polygon(
-                    points, fill="", outline=ROADLINE_COLOR_MAP[road_line.type]
+                    points, fill="", outline=ROADLINE_COLOR_MAP[line.type]
                 )
                 continue
             if cur_min_idx > 0:
                 cur_min_idx -= 1
-            if cur_max_idx < len(road_line.points) - 1:
+            if cur_max_idx < len(line.points) - 1:
                 cur_max_idx += 1
             points = [
                 convert_pos(Vector2(pt.x, pt.y))
-                for pt in road_line.points[cur_min_idx : cur_max_idx + 1]
+                for pt in line.points[cur_min_idx : cur_max_idx + 1]
             ]
-            map_canvas.create_line(points, fill=ROADLINE_COLOR_MAP[road_line.type])
-
-        # # 绘制停车位
-        # parkinglot = message["perceptive_parkinglot"]
-        # # 没有停车位时，perceptive_parkinglot 的所有值都为 0
-        # if parkinglot["size_length"] != 0 and parkinglot["size_width"] != 0:
-        #     pos = Vector2(parkinglot["pos_x"], parkinglot["pos_y"])
-        #     length = parkinglot["size_length"]
-        #     width = parkinglot["size_width"]
-        #     yaw = parkinglot["rot_yaw"]
-        #     draw_rectangle(
-        #         pos,
-        #         length,
-        #         width,
-        #         yaw_to_radians(yaw),
-        #         "dark green",
-        #     )
+            map_canvas.create_line(points, fill=ROADLINE_COLOR_MAP[line.type])
 
     def check_message():
         """检查管道，如果有消息则处理。"""
@@ -165,7 +157,7 @@ def tk_process_func(
             message = conn.recv()
             match message["type"]:
                 case Message.UPDATE:
-                    update(message["value"])
+                    update(message["msg"])
                 case Message.QUIT:
                     root.destroy()
 
@@ -174,13 +166,26 @@ def tk_process_func(
         next_interval = max(0, refresh_interval - int(elapsed_time * 1000))
         root.after(next_interval, check_message)
 
+    # 先把所有需要绘制的车道线提取出来
+    line_list: list[LineInfo] = []
+    for road_line in scene_static_data.road_lines:
+        if road_line.stop_line:
+            line_list.append(LineInfo(LineType.STOP_LINE, road_line.stop_line))
+        for lane in road_line.lane_data:
+            line_list.append(
+                LineInfo(lane.left_border.border_type, lane.left_border.path_point)
+            )
+            line_list.append(
+                LineInfo(lane.right_border.border_type, lane.right_border.path_point)
+            )
+
     # 建 kdtree
-    road_lines_points = []
-    point_info = []
-    for road_line_idx, road_line in enumerate(road_lines):
-        for point_idx, point in enumerate(road_line.points):
+    road_lines_points: list[tuple[float, float]] = []
+    point_info: list[tuple[int, int]] = []
+    for line_idx, line in enumerate(line_list):
+        for point_idx, point in enumerate(line.points):
             road_lines_points.append((point.x, point.y))
-            point_info.append((road_line_idx, point_idx))
+            point_info.append((line_idx, point_idx))
     kdtree = KDTree(road_lines_points)
 
     root = Tk()
@@ -231,14 +236,16 @@ class Dashboard:
     使用多进程实现，使用 multiprocessing.Pipe 作为进程间通信的方式。
     """
 
-    def __init__(self, road_lines: list[RoadLineInfo], refresh_interval: int = 20):
+    def __init__(self, scene_static_data: SceneStaticData, refresh_interval: int = 20):
         """
-        refresh_interval: 刷新间隔，单位毫秒，一定要比 update 方法的调用频率高。
+        初始化仪表盘。
+        :param scene_static_data: 场景静态数据，用于绘制车道线。
+        :param refresh_interval: 刷新间隔，单位毫秒，一定要比 update 方法的调用频率高。
         """
         self._conn, child_conn = Pipe()
         self._tk_process = Process(
             target=tk_process_func,
-            args=(child_conn, road_lines, refresh_interval),
+            args=(child_conn, scene_static_data, refresh_interval),
         )
         self._tk_process.start()
         # 等待仪表盘初始化完成（包括创建窗口、创建 kdtree 等）
@@ -246,10 +253,10 @@ class Dashboard:
         if ready != "ready":
             raise RuntimeError("仪表盘初始化失败")
 
-    def update(self, scene_info: SceneInfo):
+    def update(self, msg: SimCarMsg):
         """更新仪表盘的方法。"""
         if self._tk_process.is_alive():
-            self._conn.send({"type": Message.UPDATE, "value": scene_info})
+            self._conn.send({"type": Message.UPDATE, "msg": msg})
 
     def quit(self):
         """退出仪表盘的方法。"""
--- examples/main.py
+++ examples/main.py
@@ -1,26 +1,23 @@
 import logging
 import keyboard
 import math
-from scene_api import (
-    SceneAPI,
-    VehicleControl,
-    GearMode,
-    SceneInfo,
-    ConnectionClosedError,
-    Vector3,
-    ObjectInfo,
-)
+from metacar import SceneAPI, GearMode, VehicleControl, Vector3, SimCarMsg
 
 USE_GUI = True
 
 if USE_GUI:
-    from gui import Dashboard
+    from examples.gui import Dashboard
 
-logging.basicConfig(filename="autodrive.log", level=logging.DEBUG, encoding="utf-8")
+logging.basicConfig(
+    filename="autodrive.log",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.DEBUG,
+    encoding="utf-8",
+)
 logger = logging.getLogger(__name__)
 
 current_gear = GearMode.DRIVE
-use_keyboard = False
+use_keyboard = True
 
 
 def set_gear(gear: GearMode):
@@ -33,7 +30,7 @@ def toggle_keyboard():
     use_keyboard = not use_keyboard
 
 
-def get_vehicle_control_from_keyboard(scene_info: SceneInfo) -> VehicleControl:
+def get_vehicle_control_from_keyboard() -> VehicleControl:
     vc = VehicleControl()
     vc.gear = current_gear
     if keyboard.is_pressed("up") or keyboard.is_pressed("w"):
@@ -68,35 +65,37 @@ def calc_throttle_brake(
 
 # Stanley 算法
 def calc_steering(
-    main_vehicle_info: ObjectInfo, speed: float, trajectory: list[Vector3]
+    pos: Vector3, yaw: float, speed: float, trajectory: list[Vector3]
 ) -> float:
-    if len(trajectory) < 3:
-        return 0
     K = 0.5
-    # trajectory 的最后两个点都没什么用
-    for pos in trajectory[:-2]:
-        if math.dist(pos, main_vehicle_info.pos) > K * speed:
-            target_pos = pos
+    for traj_pos in trajectory:
+        if math.dist(traj_pos, pos) > K * speed:
+            target_pos = traj_pos
             break
     else:
-        target_pos = trajectory[-3]
-    theta = (target_pos - main_vehicle_info.pos).yaw_rad()
-    steering_angle = (theta - main_vehicle_info.yaw) % (2 * math.pi)
+        target_pos = trajectory[-1]
+    theta = (target_pos - pos).yaw_rad()
+    steering_angle = (theta - yaw) % (2 * math.pi)
     if steering_angle > math.pi:
         steering_angle -= 2 * math.pi
-    steering = math.degrees(-steering_angle) / 45 * 2  # 这个 2 是一个调整参数
+    steering = math.degrees(-steering_angle) / 45 * 2  # 2 是一个可调参数
     return max(min(steering, 1), -1)
 
 
-def get_vehicle_control_from_algorithm(scene_info: SceneInfo) -> VehicleControl:
+def get_vehicle_control_from_algorithm(msg: SimCarMsg) -> VehicleControl:
     vc = VehicleControl()
     vc.gear = GearMode.DRIVE
     # 目前只支持固定速度
-    vc.throttle, vc.brake = calc_throttle_brake(scene_info.main_vehicle_speed, 15)
+    vc.throttle, vc.brake = calc_throttle_brake(msg.data_main_vehicle.speed, 15)
     vc.steering = calc_steering(
-        scene_info.main_vehicle_info,
-        scene_info.main_vehicle_speed,
-        scene_info.trajectory.points,
+        Vector3(
+            msg.data_gnss.pose_gnss.pos_x,
+            msg.data_gnss.pose_gnss.pos_y,
+            msg.data_gnss.pose_gnss.pos_z,
+        ),
+        -math.radians(msg.data_gnss.pose_gnss.ori_z),
+        msg.data_main_vehicle.speed,
+        msg.trajectory,
     )
     return vc
 
@@ -110,34 +109,25 @@ def main():
     keyboard.add_hotkey("r", lambda: set_gear(GearMode.REVERSE))
     keyboard.add_hotkey("f", lambda: set_gear(GearMode.DRIVE))
     keyboard.add_hotkey("t", lambda: set_gear(GearMode.NEUTRAL))
-    keyboard.add_hotkey("g", lambda: set_gear(GearMode.PARK))
+    keyboard.add_hotkey("g", lambda: set_gear(GearMode.PARKING))
 
     keyboard.add_hotkey("c", toggle_keyboard)
 
     api.connect()
+    static_data = api.get_scene_static_data()
     if USE_GUI:
-        dashboard = Dashboard(api.get_road_lines())
+        dashboard = Dashboard(static_data)
         logger.info("启动 GUI 界面")
-    api.ready()
     logger.info("开始场景")
 
-    while True:
-        try:
-            scene_info = api.get_scene_info()
-        except ConnectionClosedError:
-            logger.warning("连接中断，退出场景")
-            break
-        # scene_info 为 None 时表示场景结束
-        if scene_info is None:
-            break
+    for sim_car_msg, frame in api.main_loop():
         if use_keyboard:
-            vehicle_control = get_vehicle_control_from_keyboard(scene_info)
+            vehicle_control = get_vehicle_control_from_keyboard()
         else:
-            vehicle_control = get_vehicle_control_from_algorithm(scene_info)
+            vehicle_control = get_vehicle_control_from_algorithm(sim_car_msg)
         api.set_vehicle_control(vehicle_control)
-
         if USE_GUI:
-            dashboard.update(scene_info)
+            dashboard.update(sim_car_msg)
     logger.info("结束场景")
     if USE_GUI:
         dashboard.quit()
